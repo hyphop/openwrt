08b5a775b802d9c9a8577b58aec0fe3108ffc1da
GMB004 <126212111+GMB004@users.noreply.github.com>
2023-02-23 21:25:12 -0300

    Add TP-Link BR id

--- tools/firmware-utils/src/tplink-safeloader.c
+++ tools/firmware-utils/src/tplink-safeloader.c
@@ -817,8 +817,7 @@ static struct device_info boards[] = {
 		.support_list =
 			"SupportList:\r\n"
 			"{product_name:Archer C60,product_ver:1.0.0,special_id:00000000}\r\n"
-			"{product_name:Archer C60,product_ver:1.0.0,special_id:45550000}\r\n"
-			"{product_name:Archer C60,product_ver:1.0.0,special_id:55530000}\r\n",
+			"{product_name:Archer C60,product_ver:1.0.0,special_id:42520000}\r\n"
 		.support_trail = '\x00',
 		.soft_ver = "soft_ver:1.0.0\n",
 
